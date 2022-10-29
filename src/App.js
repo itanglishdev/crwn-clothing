755c8f816557a996819b68bf6d02f19fb9e9f957
Chris <chrishethertonweb@gmail.com>
2022-10-29 15:51:06 +0200

    added items

--- src/App.js
+++ src/App.js
@@ -6,6 +6,35 @@ const  App =() => {
       {/* <img/> */}
       <div>
         <h2>Hats</h2>
+        <p>shop now</p>
+      </div>
+     </div>
+     <div className="category-container">
+      {/* <img/> */}
+      <div>
+        <h2>jackets</h2>
+        <p>shop now</p>
+      </div>
+     </div>
+     <div className="category-container">
+      {/* <img/> */}
+      <div>
+        <h2>sneakers</h2>
+        <p>shop now</p>
+      </div>
+     </div>
+     <div className="category-container">
+      {/* <img/> */}
+      <div>
+        <h2>womens</h2>
+        <p>shop now</p>
+      </div>
+     </div>
+     <div className="category-container">
+      {/* <img/> */}
+      <div>
+        <h2>mens</h2>
+        <p>shop now</p>
       </div>
      </div>
     </div>
