25f3656aca910b301d42ba868c1e1cb642dd978f
Chris <chrishethertonweb@gmail.com>
2022-10-30 11:02:06 +0100

    added sign in components and firebase libraries put in

--- src/routes/sign-in/sign-in.components.js
+++ src/routes/sign-in/sign-in.components.js
@@ -5,7 +5,7 @@ const SignIn = () => {
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup()
         // console.log(response);
-        createUserDocumentFromAuth(user)
+        const userDocRef = await createUserDocumentFromAuth(user)
 }
 
   return (
